e69ec4cf3ca9c2949a4998aaaacd94ffc9a98f1e
Jorciney <jorciney.dias.chaveiro@telenetgroup.be>
2023-11-09 18:33:27 +0100

    adding paginator

--- src/app/ui/consent-table.component.ts
+++ src/app/ui/consent-table.component.ts
@@ -2,23 +2,27 @@ import { Component, Input } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { NgForOf } from '@angular/common';
 import { TableMetadata } from '../data-access/model/table';
+import { PaginationComponent } from './pagination.component';
 
 @Component({
   standalone: true,
   selector: 'consent-flow-consent-table',
   template: `
-    <table mat-table [dataSource]="dataSource || []" class="mat-elevation-z8">
-      <ng-container *ngFor="let metadata of metadataList">
-        <ng-container [matColumnDef]="metadata.prop">
-          <th mat-header-cell *matHeaderCellDef>{{ metadata.title }}</th>
-          <td mat-cell *matCellDef="let element">{{ element[metadata.prop] }}</td>
+    <div class="flex flex-col gap-2 justify-center items-center w-full">
+      <table mat-table [dataSource]="dataSource || []" class="mat-elevation-z8">
+        <ng-container *ngFor="let metadata of metadataList">
+          <ng-container [matColumnDef]="metadata.prop">
+            <th mat-header-cell *matHeaderCellDef>{{ metadata.title }}</th>
+            <td mat-cell *matCellDef="let element">{{ element[metadata.prop] }}</td>
+          </ng-container>
         </ng-container>
-      </ng-container>
-      <tr mat-header-row *matHeaderRowDef="columnNames"></tr>
-      <tr mat-row *matRowDef="let row; columns: columnNames"></tr>
-    </table>
+        <tr mat-header-row *matHeaderRowDef="columnNames"></tr>
+        <tr mat-row *matRowDef="let row; columns: columnNames"></tr>
+      </table>
+      <consent-flow-pagination class="w-full" [totalPages]="(dataSource?.length || 0) / 2"></consent-flow-pagination>
+    </div>
   `,
-  imports: [MatTableModule, NgForOf],
+  imports: [MatTableModule, NgForOf, PaginationComponent],
 })
 export class ConsentTableComponent {
   @Input() dataSource: unknown[] | null;
--- src/app/ui/pagination.component.ts
+++ src/app/ui/pagination.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { NgClass, NgForOf } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'consent-flow-pagination',
-  template: ` <div>Pagination works!</div> `,
+  template: `
+    <div class="w-full flex flex-row justify-between items-center gap-5 text-blue-600">
+      <a (click)="previousClick.emit()" class="p-1 cursor-pointer"><< Previous page</a>
+      <div class="flex flex-row">
+        <ng-container *ngFor="let page of [].constructor(totalPages); let i = index">
+          <div
+            (click)="pageClick.emit(page)"
+            class="p-1 cursor-pointer"
+            [ngClass]="{ 'font-bold': currentPage === i + 1 }">
+            {{ i + 1 }}
+          </div>
+        </ng-container>
+      </div>
+      <a (click)="nextClick.emit()" class="p-1 cursor-pointer">Next page >></a>
+    </div>
+  `,
+  imports: [NgForOf, NgClass],
 })
-export class PaginationComponent {}
+export class PaginationComponent {
+  @Output() previousClick = new EventEmitter<void>();
+  @Output() nextClick = new EventEmitter<void>();
+  @Output() pageClick = new EventEmitter<number>();
+  @Input() currentPage: number;
+  @Input() totalPages?: number;
+}
