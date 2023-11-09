774a052afc5e9fdc354fdb94f41bf5d99cb96a68
Jorciney <jorciney.dias.chaveiro@telenetgroup.be>
2023-11-09 19:11:48 +0100

    adding condition for previous and next page

--- src/app/ui/pagination.component.ts
+++ src/app/ui/pagination.component.ts
@@ -1,13 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { NgClass, NgForOf } from '@angular/common';
+import { NgClass, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'consent-flow-pagination',
   template: `
     <div class="w-full flex flex-row justify-between items-center gap-5 text-blue-600">
-      <a (click)="previousClick.emit()" class="p-1 cursor-pointer"><< Previous page</a>
+      <a *ngIf="currentPage !== 1" (click)="previousClick.emit()" class="mr-40 p-1 cursor-pointer"><< Previous page</a>
       <div class="flex flex-row">
         <ng-container *ngFor="let page of [].constructor(totalPages); let i = index">
           <div
@@ -18,15 +18,15 @@ import { NgClass, NgForOf } from '@angular/common';
           </div>
         </ng-container>
       </div>
-      <a (click)="nextClick.emit()" class="p-1 cursor-pointer">Next page >></a>
+      <a *ngIf="currentPage !== totalPages" (click)="nextClick.emit()" class="ml-40 p-1 cursor-pointer">Next page >></a>
     </div>
   `,
-  imports: [NgForOf, NgClass],
+  imports: [NgForOf, NgClass, NgIf],
 })
 export class PaginationComponent {
   @Output() previousClick = new EventEmitter<void>();
   @Output() nextClick = new EventEmitter<void>();
   @Output() pageClick = new EventEmitter<number>();
-  @Input() currentPage: number;
+  @Input() currentPage: number = 0;
   @Input() totalPages?: number;
 }
