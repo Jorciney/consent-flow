a2b3111bd2b69b73396287c9513fc16207deca29
Jorciney <jorciney.dias@hotmail.com>
2023-11-11 21:48:47 +0100

    trying to fix e2e job

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -1,10 +1,14 @@
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 import { defineConfig } from 'cypress';
 
+const e2e = nxE2EPreset(__filename, { cypressDir: 'src'});
 export default defineConfig({
-  e2e: nxE2EPreset(__filename, { cypressDir: 'src' }),
+  e2e: {
+    ...e2e,
+  baseUrl:'http://localhost:4200',
+  },
+  retries: 2,
   viewportWidth: 1920,
   viewportHeight: 1080,
-  projectId: 'e2e',
   video: true
 });
